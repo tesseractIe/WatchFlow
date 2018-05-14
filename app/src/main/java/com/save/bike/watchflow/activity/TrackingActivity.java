d13af97623495764fdf8e9d6aa3d8c00818568f7
tesseract <tsr.c.dc@gmail.com>
2018-05-14 18:04:03 +0300

    commit2

--- app/src/main/java/com/save/bike/watchflow/activity/TrackingActivity.java
+++ app/src/main/java/com/save/bike/watchflow/activity/TrackingActivity.java
@@ -1,6 +1,10 @@
 package com.save.bike.watchflow.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.wear.widget.WearableRecyclerView;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.ListView;
 
@@ -12,24 +16,41 @@ import java.util.ArrayList;
 
 public class TrackingActivity extends WearableActivity {
 
-    ListView listView;
+    WearableRecyclerView listView;
+    Context context;
+    ArrayList<MenuItem> listItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking);
         listView = findViewById(R.id.activity_main_list_view);
+        context = this;
 
-        ArrayList<MenuItem> listItems = new ArrayList<>();
+        listItems = new ArrayList<>();
         listItems.add(new MenuItem("Blood sugar","Last://////","2011-11-11 20:20:13",HeartRateActivity.class));
         listItems.add(new MenuItem("Weight","Last://////","2011-11-11 20:20:13",HeartRateActivity.class));
         listItems.add(new MenuItem("Heart Rate","Last://////","2011-11-11 20:20:13",HeartRateActivity.class));
         listItems.add(new MenuItem("Activity","Last://////","2011-11-11 20:20:13",HeartRateActivity.class));
         listItems.add(new MenuItem("Mood","Last://////","2011-11-11 20:20:13",HeartRateActivity.class));
-        MenuItemsAdapter adapter = new MenuItemsAdapter(listItems,this);
+        MenuItemsAdapter adapter = new MenuItemsAdapter(listItems);
+        adapter.setListener(new MenuItemsAdapter.ItemSelectedListener() {
+            @Override
+            public void onItemSelected(int position) {
+                startAct(position);
+            }
+        });
+        listView.setEdgeItemsCenteringEnabled(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        listView.setLayoutManager(llm);
         listView.setAdapter(adapter);
 
         setAmbientEnabled();
+    }
+
+    private void startAct(int pos){
+        startActivity(new Intent(context,listItems.get(pos).launchActivity));
     }
 
 }
--- app/src/main/java/com/save/bike/watchflow/adapter/MenuItemsAdapter.java
+++ app/src/main/java/com/save/bike/watchflow/adapter/MenuItemsAdapter.java
@@ -1,11 +1,9 @@
 package com.save.bike.watchflow.adapter;
 
-import android.content.Context;
-import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.save.bike.watchflow.MenuItem;
@@ -13,51 +11,65 @@ import com.save.bike.watchflow.R;
 
 import java.util.ArrayList;
 
-public class MenuItemsAdapter extends BaseAdapter {
+public class MenuItemsAdapter extends RecyclerView.Adapter<MenuItemsAdapter.ViewHolder> {
+    private ArrayList<MenuItem> data;
+    private ItemSelectedListener itemSelectedListener;
 
-    private ArrayList<MenuItem> items;
-    private Context context;
-
-    public MenuItemsAdapter(ArrayList<MenuItem> items, Context context) {
-        this.items = items;
-        this.context = context;
+    public MenuItemsAdapter(ArrayList<MenuItem> data) {
+        this.data = data;
     }
 
-    @Override
-    public int getCount() {
-        return items.size();
-    }
-
-    @Override
-    public MenuItem getItem(int position) {
-        return items.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View view = convertView;
-        if (view == null) {
-            view = LayoutInflater.from(context).inflate(R.layout.item_list_activity_main, parent, false);
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView textTitle;
+        private TextView textStatus;
+        private TextView textDate;
+        ViewHolder(View view) {
+            super(view);
+            textTitle = view.findViewById(R.id.text_view_title);
+            textStatus = view.findViewById(R.id.text_view_status);
+            textDate = view.findViewById(R.id.text_view_date);
         }
+        void bind(final int position, final ItemSelectedListener listener) {
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null) {
+                        listener.onItemSelected(position);
+                    }
+                }
+            });
+        }
+    }
 
-        final MenuItem p = getItem(position);
+    public void setListener(ItemSelectedListener itemSelectedListener) {
+        this.itemSelectedListener = itemSelectedListener;
+    }
 
-        // заполняем View в пункте списка данными из товаров: наименование, цена
-        // и картинка
-        ((TextView) view.findViewById(R.id.text_view_title)).setText(p.title);
-        ((TextView) view.findViewById(R.id.text_view_status)).setText(p.status);
-        ((TextView) view.findViewById(R.id.text_view_date)).setText(p.date);
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                context.startActivity(new Intent(context, p.launchActivity));
-            }
-        });
-        return view;
+    @Override
+    public MenuItemsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list_activity_main, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(MenuItemsAdapter.ViewHolder holder, final int position) {
+        if (data != null && !data.isEmpty()) {
+            holder.textDate.setText(data.get(position).date);
+            holder.textStatus.setText(data.get(position).status);
+            holder.textTitle.setText(data.get(position).title);
+            holder.bind(position, itemSelectedListener);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (data != null && !data.isEmpty()) {
+            return data.size();
+        }
+        return 0;
+    }
+
+    public interface ItemSelectedListener {
+        void onItemSelected(int position);
     }
 }
